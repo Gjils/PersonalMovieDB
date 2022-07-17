d7a227477b54f5777184f0620aa94cfd4fd3f93a
Gjils <preobrazhenskyalexsey@gmail.com>
2022-07-18 01:31:44 +0300

    Created Personal Movie DB and two films adder

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let filmName1 = prompt("Один из последних просмотренных фильмов?", ""),
+  filmRate1 = +prompt("На сколько оцените его?", ""),
+  filmName2 = prompt("Один из последних просмотренных фильмов?", ""),
+  filmRate2 = +prompt("На сколько оцените его", "");
+
+personalMovieDB.movies[filmName1] = filmRate1;
+personalMovieDB.movies[filmName2] = filmRate2;
+
+console.log(personalMovieDB);
